aa259dcc9990ba8624a642fd0c245a7881a80df6
Zachary Droge <zdroge@MacBook-Pro-3.local>
2021-10-07 11:38:10 -0500

    Post lecture with added code to main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-
+import React from 'react'
 import './App.css';
+import axios from 'axios'
+import Restaurant from './Restaurant'
+const urlEndpointForApi = 'https://random-data-api.com/api/restaurant/random_restaurant'
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      title: "Random Restaurants",
+      data: {}
+    }
+  }
 
-    </div>
-  );
+
+  async componentDidMount(){
+    // This is a good place for api calls
+    let {data} = await axios.get(urlEndpointForApi)
+    console.log("Res: ", data)
+    this.setState({data: data})
+
+  }
+
+
+  render(){
+    // this is a place for some JS
+    return (
+      // JSX land
+      <>
+        <div>{this.state.title}</div>
+        {/* <div>{JSON.stringify(this.state.data)}</div> */}
+
+
+        <Restaurant resData={this.state.data} />
+
+      </>
+
+    )
+  }
+
+
+
+
 }
 
-export default App;
+export default App
+
+
